2a470bf08c34e22aeac767139d287ca1209d6da1
Dylan O'Dowd <Dylan O'Dowd>
2017-02-02 11:10:59 -0500

    added confirm command, changed math to be correct and between 1 and 10, changed variable names to be more easily read, and corrected order of operations parenthesis

--- script.js
+++ script.js
@@ -1,35 +1,43 @@
 var adventureButton = document.getElementById('AB')
 
 adventureButton.addEventListener('click', function () {
-  var choice = window.prompt('Do you want to enter the cave? yes or no?')
-  choice = choice || ''
-  choice = choice.trim().toLowerCase()
-  if (choice === 'yes') {
-    choice = window.prompt('Adevnture!!!! You come to 3 tunnels. one, two, or three?')
-    choice = choice || ''
-    choice = choice.trim().toLowerCase()
-    if (choice === 'two') {
-      choice = window.prompt('You encounter a dragon! pick a number between 1 and 10!')
-      choice = choice || ''
-      if (isNaN(choice)) {
-        window.alert('Fine don\'t enter a number the dragon eats you!')
-      } else if ((choice > 10) || (choice < 0)) {
-        window.alert('That number isn\'t between 10 and 0 the dragon eats you!')
-      } else {
-        var rando = Math.random()
-        rando = rando * 10
-        if (choice >= rando) {
-          window.alert('You defeated the dragon and get all the treasure!')
+  var enterCave = window.prompt('Do you want to enter the cave? yes or no?')
+  enterCave = enterCave || ''
+  enterCave = enterCave.trim().toLowerCase()
+  var areYouSure = window.confirm('Are you sure?')
+  if (areYouSure === true) {
+    if (enterCave === 'yes') {
+      var tunnelChoice = window.prompt('Adevnture!!!! You come to 3 tunnels. one, two, or three?')
+      tunnelChoice = tunnelChoice || ''
+      tunnelChoice = tunnelChoice.trim().toLowerCase()
+      if (tunnelChoice === 'two') {
+        var numberChoice = window.prompt('You encounter a dragon! pick a number between 1 and 10!')
+        numberChoice = numberChoice || ''
+        if (isNaN(numberChoice)) {
+          window.alert('Fine don\'t enter a number the dragon eats you!')
+        } else if (numberChoice > 10 || numberChoice < 1) {
+          window.alert('That number isn\'t between 1 and 10 the dragon eats you!')
         } else {
-          window.alert('the dragon wins sorry.')
+          var rando = Math.floor(Math.random() * 10) + 1
+          if (numberChoice >= rando) {
+            window.alert('You defeated the dragon and get all the treasure!')
+          } else {
+            window.alert('the dragon wins sorry.')
+          }
         }
+      } else if (tunnelChoice === 'one') {
+        window.alert('You found a magical sword!')
+      } else {
+        window.alert('You fell into a pit trap. Adventure OVER!!')
       }
-    } else if (choice === 'one') {
-      window.alert('You found a magical sword!')
     } else {
-      window.alert('You fell into a pit trap. Adventure OVER!!')
+      window.alert('Fine no Adventure for you!')
     }
   } else {
-    window.alert('Fine no Adventure for you')
+    if (enterCave === 'yes') {
+      window.alert('Fine no Adventure for you')
+    } else {
+      window.alert('To late no adventure for you')
+    }
   }
 })
